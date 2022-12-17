a030c7363e076b655bedd7d3df5eeb6b700d6b9e
Tolik <anatolijparygin988@gmail.com>
2022-12-17 21:02:59 +0300

    Сумма элементов главной диагонали

---
+++ zadacha_4/Program.cs
@@ -0,0 +1,53 @@
+﻿Console.Clear();
+
+int EnterNumber(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int[,] CreateRandomArray(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(leftRange, rightRange);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int m = EnterNumber("Введите значение m: ");
+int n = EnterNumber("Введите значение n: ");
+int[,] matrix = CreateRandomArray(m, n, 1, 10);
+PrintArray(matrix);
+System.Console.WriteLine();
+int sumElementsDioganal = 0;
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if (i == j)
+        {
+            sumElementsDioganal = sumElementsDioganal + matrix[i, j];
+        }
+
+    }
+}
+System.Console.WriteLine("Сумма элементов главной диагонали = "+sumElementsDioganal);
