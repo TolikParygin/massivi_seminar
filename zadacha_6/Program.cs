73f2cd9a7703549221786c89ed73f046dcfd53cb
Tolik <anatolijparygin988@gmail.com>
2022-12-19 18:47:07 +0300

    меняем местами строки и столбцы

---
+++ zadacha_6/Program.cs
@@ -0,0 +1,69 @@
+﻿// 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+Console.Clear();
+
+void ModifiedPrintArray(int[,] array)
+{
+        if (array.GetLength(0) == array.GetLength(1))
+
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                int temp = array[i, j];
+                array[i, j] = array[j,i];
+                array[j,i] = temp;
+            }
+    }
+    }
+    else
+    {
+        System.Console.WriteLine("нельзя заменить строки на столбцы");
+
+    }
+
+    
+}
+
+int EnterNumber(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int[,] CreateRandomArray(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(leftRange, rightRange);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = EnterNumber("Введите значение m: ");
+int n = EnterNumber("Введите значение n: ");
+int[,] matrix = CreateRandomArray(m, n, 1, 10);
+System.Console.WriteLine();
+PrintArray(matrix);
+System.Console.WriteLine();
+ModifiedPrintArray(matrix);
+PrintArray(matrix);
